0e1dcf308d639c2b04acbfa463ba372a84edd3bb
MatthijsvanderPlas <mvdplas84@ziggo.nl>
2020-01-03 14:59:01 +0100

    improved on the function

--- JPGtoPNGconverter.py
+++ JPGtoPNGconverter.py
@@ -2,20 +2,20 @@ from PIL import Image
 import sys
 import os
 
+pathfile = sys.argv[1]
+outputpath = sys.argv[2]
+
 
 def convert():
-    Path = sys.argv[1]
-    if os.path.exists(sys.argv[2] ):
-        pass
-    else:
-        os.mkdir(f".\{sys.argv[2]}")
+    if not os.path.exists(outputpath):
+        os.mkdir(f".\{outputpath}")
 
-    filelist = os.listdir(Path)
+    filelist = os.listdir(pathfile)
     for x in filelist:
         if x.endswith(".jpg"):
-            im = Image.open(Path + x)
-            tmp = x.split(".")
-            im.save(sys.argv[2] + tmp[0]+".png", "png")
+            im = Image.open(pathfile + x)
+            tmp = os.path.splitext(x)
+            im.save(outputpath + tmp[0] + ".png", "png")
 
 
 if __name__ == '__main__':
